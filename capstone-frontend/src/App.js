00a4b8a2101439a371e288d070cf845eb3d1c6b2
Steven <steven.j.solar@gmail.com>
2020-06-23 18:13:34 +0000

    remove unused import

--- capstone-frontend/src/App.js
+++ capstone-frontend/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import CreateClub from './components/CreateClub.js';
-
 function App() {
   return (
     <div className="App">
