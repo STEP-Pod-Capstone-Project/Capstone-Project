0115e4dae680f0439a78eab66b67dc7c5185fa1d
Steven <steven.j.solar@gmail.com>
2020-06-23 18:10:54 +0000

    basic frontend form to create a club

--- capstone-frontend/src/App.js
+++ capstone-frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import CreateClub from './components/CreateClub.js';
+
 function App() {
   return (
     <div className="App">
