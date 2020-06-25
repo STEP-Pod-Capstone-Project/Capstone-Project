eb763260cfa4b3bf29fdab3cd0f38774958163ac
Steven <steven.j.solar@gmail.com>
2020-06-25 14:44:32 +0000

    makes right side bar a functional stateless component

--- capstone-frontend/src/components/RightSideBar.js
+++ capstone-frontend/src/components/RightSideBar.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-class RightSideBar extends Component {
-  render() {
-    return (
-      <div id="right-sidebar-container" className="sidebar-expanded d-none d-md-block col-2 sidebar-container">
-        Space for a user feed!
-      </div>
-    );
-  }
-}
+const RightSideBar = () => {
+  return (
+    <div id="right-sidebar-container" className="sidebar-expanded d-none d-md-block col-2 sidebar-container">
+      Space for a user feed!
+    </div>
+  );
+};
 
 export default RightSideBar;
