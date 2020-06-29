a0f842846cb8f6764fd9255f01dbacb9148a16ce
Steven <steven.j.solar@gmail.com>
2020-06-29 20:03:46 +0000

    fixes imports

--- capstone-frontend/src/components/MyClubs.jsx
+++ capstone-frontend/src/components/MyClubs.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import ClubGridItem from './ClubGridItem.js';
+import ClubGridItem from './ClubGridItem.jsx';
 
 import '../styles/Clubs.css';
 
