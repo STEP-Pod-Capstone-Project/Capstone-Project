0afea961798893601ad9018149708a5357dbf1ec
Steven <steven.j.solar@gmail.com>
2020-07-01 04:54:59 +0000

    clubpage fetches data

--- capstone-frontend/src/components/ClubPage.jsx
+++ capstone-frontend/src/components/ClubPage.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class ClubPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: {}
+    }
+  }
+
+  fetchClubData = () => {
+    fetch(`/api/clubs?id=${this.props.match.params.id}`)
+        .then(response => response.json()).then(res => this.setState({response: res[0]}));
+  }
+
   render() {
+    this.fetchClubData();
     return (
       <div>
-        <div> {this.props.match.params.id} </div>
+        <div> Club Name: {this.state.response.name} </div>
+        <div> ID: {this.props.match.params.id} </div>
+        <div> Description: {this.state.response.description} </div>
+        <div> OwnerID: {this.state.response.ownerID} </div>
+        <div> GbookID: {this.state.response.gbookID} </div>
       </div>
     );
   }
