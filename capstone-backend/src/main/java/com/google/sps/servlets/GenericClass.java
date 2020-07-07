44a9e705915604cbcc613a2ed23af3f12bc8de72
Steven <steven.j.solar@gmail.com>
2020-07-07 18:15:52 +0000

    forgot to remove id variable from GenericClass<T>

--- capstone-backend/src/main/java/com/google/sps/servlets/GenericClass.java
+++ capstone-backend/src/main/java/com/google/sps/servlets/GenericClass.java
@@ -3,7 +3,6 @@ package com.google.sps.servlets;
 public class GenericClass<T> {
 
   private final Class<T> type;
-  private String id;
 
   public GenericClass(Class<T> type) {
     this.type = type;
