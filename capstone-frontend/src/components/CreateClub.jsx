5f27cc0e9e7e760dd44d32655b8f9c79b51a2aac
Steven <steven.j.solar@gmail.com>
2020-07-08 22:55:42 +0000

    respond to pr comments, fix put bug

--- capstone-frontend/src/components/CreateClub.jsx
+++ capstone-frontend/src/components/CreateClub.jsx
@@ -9,7 +9,8 @@ class CreateClub extends Component {
     let data = {};
     const formElements = document.getElementById("create-club-form").elements;
     for (let i = 0; i < formElements.length; i++) {
-      if (formElements[i].type !== "submit") {
+      if (formElements[i].name.length != 0 && 
+          formElements[i].type !== "submit" && formElements[i].value.length != 0) {
         data[formElements[i].name] = formElements[i].value;
       }
     }
