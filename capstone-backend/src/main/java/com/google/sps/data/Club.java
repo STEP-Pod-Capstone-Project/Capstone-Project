1f3f8b9655b00a6063f9b7325298720706eb5f42
Steven <steven.j.solar@gmail.com>
2020-07-10 15:58:37 +0000

    modify clubs to have assignments

--- capstone-backend/src/main/java/com/google/sps/data/Club.java
+++ capstone-backend/src/main/java/com/google/sps/data/Club.java
@@ -32,7 +32,99 @@ public class Club extends Group {
   public Club() {
     this("", "");
   }
+
+  public String getName() {
+    return this.name;
+  }
  
+  public void setName(String name) {
+    this.name = name;
+  }
+ 
+  public String getDescription() {
+    return this.description;
+  }
+ 
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getAnnouncement() {
+    return this.announcement;
+  }
+ 
+  public void setAnnouncement(String announcement) {
+    this.announcement = announcement;
+  }
+
+  public List<Assignment> getAssignments() {
+    return this.assignments;
+  }
+
+  public void addAssignment(Assignment assignment) {
+    this.assignments.add(assignment);
+  }
+
+  public void clearAssignments() {
+    this.assignments.clear();
+  }
+
+  public String getOwnerID() {
+    return ownerID;
+  }
+
+  public void setOwnerID(String ownerID) {
+    this.ownerID = ownerID;
+  }
+ 
+  public List<String> getMemberIDs() {
+    return this.memberIDs;
+  }
+
+  public void removeMember(String memberID) {
+    memberIDs.remove(memberID);
+  }
+
+  public void removeMembers(List<String> memberIDs) {
+    for (String m : memberIDs) {
+      this.memberIDs.remove(m);
+    }
+  }
+ 
+  public List<String> getInviteIDs() {
+    return this.inviteIDs;
+  }
+
+  public void invite(String inviteID) {
+    inviteIDs.add(inviteID);
+  }
+
+  public void invite(List<String> inviteIDs) {
+    for (String i : inviteIDs) {
+      this.inviteIDs.add(i);
+    }
+  }
+
+  public void uninvite(String uninviteID) {
+    inviteIDs.remove(uninviteID);
+  }
+
+  public void uninvite(List<String> uninviteIDs) {
+    for (String u : uninviteIDs) {
+      this.inviteIDs.remove(u);
+    }
+  }
+
+  public boolean addMember(String memberID) {
+    if (inviteIDs.contains(memberID)) {
+      inviteIDs.remove(memberID);
+      memberIDs.add(memberID);
+      return true;
+    }
+    return false;
+  }
+
+>>>>>>> modify clubs to have assignments
   public String getGbookID() {
     return this.gbookID;
   }
