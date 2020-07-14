07ffac84a4b609b74d62a15120785b58cf1a1955
Steven <steven.j.solar@gmail.com>
2020-07-14 15:24:22 +0000

    change object storage

--- capstone-backend/src/main/java/com/google/sps/data/Assignment.java
+++ capstone-backend/src/main/java/com/google/sps/data/Assignment.java
@@ -3,24 +3,18 @@ package com.google.sps.data;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Assignment extends BaseEntity {
   private String text;
   private final String clubID;
-  private List<Comment> comments;
   private final String whenCreated;
   private String whenDue;
 
-  public Assignment(String text, String clubID, List<Comment> comments, String whenCreated,
-      String whenDue) {
+  public Assignment(String text, String clubID, String whenCreated, String whenDue) {
     this.text = text;
     this.clubID = clubID;
-    this.comments = comments;
     this.whenCreated = whenCreated;
     this.whenDue = whenDue;
-  }
-
-  public Assignment(String text, String clubID, String whenCreated, String whenDue) {
-    this(text, clubID, new ArrayList<Comment>(), whenCreated, whenDue);
   }
 
   public Assignment() {
@@ -37,22 +31,6 @@ public class Assignment extends BaseEntity {
 
   public String getClubID() {
     return clubID;
-  }
-
-  public List<Comment> getComments() {
-    return comments;
-  }
-
-  public void setComments(ArrayList<Comment> comments) {
-    this.comments = comments;
-  }
-
-  public void addComment(Comment comment) {
-    this.comments.add(comment);
-  }
-
-  public void removeComment(Comment comment) {
-    this.comments.remove(comment);
   }
 
   public String getWhenCreated() {
