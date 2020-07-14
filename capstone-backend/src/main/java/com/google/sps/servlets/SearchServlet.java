1f7714d94e140a1d78e5fd4a3447caa29cb1b0dc
antoniolinhart <antonio.linhart@gmail.com>
2020-07-14 20:59:01 +0000

    Update parameter validation

--- capstone-backend/src/main/java/com/google/sps/servlets/SearchServlet.java
+++ capstone-backend/src/main/java/com/google/sps/servlets/SearchServlet.java
@@ -59,7 +59,8 @@ public class SearchServlet extends HttpServlet {
       formattedURL = String.format("https://www.googleapis.com/books/v1/volumes/%s", gbookId);
 
     } else if (request.getParameter("searchTerm") != null || !request.getParameter("searchTerm").isEmpty()) {
-      if (request.getParameterMap().size() >= 2 && request.getParameter("maxResults") == null) {
+      int parameterLength = request.getParameterMap().size();
+      if ((parameterLength == 2 && request.getParameter("maxResults") == null) || parameterLength > 2) {
         System.err.println("Error: No other parameters must be sent with searchTerm other than maxResults");
         response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         return;
