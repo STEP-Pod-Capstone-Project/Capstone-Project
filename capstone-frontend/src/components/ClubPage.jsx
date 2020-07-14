818da38b38a6029a86c6d5f8934f85aa42580716
Steven <steven.j.solar@gmail.com>
2020-07-14 14:20:01 +0000

    revert commit 5c76d575ebbb2bf9f1a3e73f41cecebead0cda56

--- capstone-frontend/src/components/ClubPage.jsx
+++ capstone-frontend/src/components/ClubPage.jsx
@@ -1,47 +1,12 @@
 import React, { Component } from 'react';
-import BookSearchTile from './BookSearchTile';
 
-import '../styles/Groups.css';
 
 class ClubPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      club: {}, 
-      owner: {}, 
-      members: [],
-      book: {}
+      club: {}
     }
-  }
-
-  fetchMemberData = () => {
-    fetch(`/api/users?id=${this.state.club.ownerID}`)
-        .then(response => response.json()).then(res => this.setState({owner: res[0]}))
-        .catch(function(err) {
-            //TODO #61: Centralize error output
-            alert(err); 
-        });
-    for (memberID in this.state.club.memberIDs) {
-      fetch(`/api/users?id=${memberID}`)
-          .then(response => response.json())
-          .then(function(res) {
-              let memberArray = this.state.members.concat(res[0]);
-              this.setState({members: memberArray})
-          })
-          .catch(function(err) {
-            //TODO #61: Centralize error output
-            alert(err); 
-          });
-    } 
-  }
-
-  fetchBookData = () => {
-    fetch(`/api/search?id=${this.state.club.gbookID}`)
-        .then(response => response.json()).then(res => this.setState({book: res[0]}))
-        .catch(function(err) {
-            //TODO #61: Centralize error output
-            alert(err);
-        });
   }
 
   fetchClubData = () => {
@@ -67,28 +32,22 @@ class ClubPage extends Component {
 
   componentDidMount() {
     this.fetchClubData();
-    this.fetchMemberData();
-    this.fetchBookData();
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.match.params.id !== prevProps.match.params.id) {
       this.fetchClubData();
-      this.fetchMemberData();
-      this.fetchBookData();
     }
   }
 
   render() {
     return (
-      <div className="text-center">
-        <div className="title"> Club Name: {this.state.club.name} </div>
-        <div> Club Owner: {this.state.owner.fullname} </div>
-        <div> Club Members: {this.state.members.map(m -> m.profileObj.fullname)} </div>
-        <div className="description"> Description: {this.state.club.description} </div>
-        <div className="description"> Current Book: </div>
-        <BookSearchTile book={this.state.book} />
-        <div> {this.state.club.assignments} <div>
+      <div>
+        <div> Club Name: {this.state.club.name} </div>
+        <div> ID: {this.props.match.params.id} </div>
+        <div> Description: {this.state.club.description} </div>
+        <div> OwnerID: {this.state.club.ownerID} </div>
+        <div> GbookID: {this.state.club.gbookID} </div>
         <button onClick={this.handleClick}> Delete </button>
       </div>
     );
