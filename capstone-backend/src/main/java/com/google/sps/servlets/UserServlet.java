d5e3f99db1b3e9736875fe038c04a16275390ae7
Mauro Guerrero <mauroguerrero20@gmail.com>
2020-07-17 17:24:07 +0000

    Fix indentation

--- capstone-backend/src/main/java/com/google/sps/servlets/UserServlet.java
+++ capstone-backend/src/main/java/com/google/sps/servlets/UserServlet.java
@@ -64,8 +64,13 @@ public class UserServlet extends HttpServlet {
         String profileImageUrl = (String) payload.get("picture");
 
         googleUser = new User(googleId, email, fullname, profileImageUrl,
-            new ImmutableMap.Builder<String, String>().put("access_token", access_token).put("idpId", idpId)
-                .put("scope", scope).put("token_id", tokenId).put("token_type", token_type).build());
+          new ImmutableMap.Builder<String, String>()
+            .put("access_token", access_token)
+            .put("idpId", idpId)
+            .put("scope", scope)
+            .put("token_id", tokenId)
+            .put("token_type", token_type)
+            .build());
 
       } else {
         System.err.println("Invalid ID token.");
