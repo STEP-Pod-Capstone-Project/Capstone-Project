575ff459cdeb0f846b4b580fb0ad874abd114572
steven-solar <steven.j.solar@gmail.com>
2020-07-21 20:33:58 +0000

    nits:

--- capstone-backend/src/main/java/com/google/sps/servlets/UserServlet.java
+++ capstone-backend/src/main/java/com/google/sps/servlets/UserServlet.java
@@ -32,6 +32,7 @@ public class UserServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
     try {
 
       JsonObject googleUserTokenObjJSON = Utility.createRequestBodyJson(request);
@@ -90,6 +91,7 @@ public class UserServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
     try {
 
       String userID = request.getParameter("id");
