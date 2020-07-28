9de6d612ccb0248ae1beb0493281e16e2db74fe6
steven-solar <steven.j.solar@gmail.com>
2020-07-28 04:14:48 +0000

    fix mobile

--- capstone-frontend/src/components/ClubGridItem.jsx
+++ capstone-frontend/src/components/ClubGridItem.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import '../styles/Groups.css';
@@ -11,18 +11,18 @@ class ClubGridItem extends Component {
       requested: this.props.club.requestIDs.includes(window.localStorage.getItem("userID"))
     }
   }
-  
+
   requestJoin = () => {
     const putBody = {
       id: this.props.club.id,
       add_requestIDs: window.localStorage.getItem("userID")
     };
-    fetch(`/api/clubs`, {method: "put", body: JSON.stringify(putBody)})
-        .then(this.setState({requested: true}))
-        .catch(function(err) {
-          //TODO #61: Centralize error output
-          alert(err);
-        });
+    fetch(`/api/clubs`, { method: "put", body: JSON.stringify(putBody) })
+      .then(this.setState({ requested: true }))
+      .catch(function (err) {
+        //TODO #61: Centralize error output
+        alert(err);
+      });
   }
 
   unRequestJoin = () => {
@@ -30,9 +30,9 @@ class ClubGridItem extends Component {
       "id": this.props.club.id,
       "remove_requestIDs": window.localStorage.getItem("userID")
     };
-    fetch(`/api/clubs`, {method: "put", body: JSON.stringify(putBody)})
-        .then(this.setState({requested: false}))
-        .catch(function(err) {
+    fetch(`/api/clubs`, { method: "put", body: JSON.stringify(putBody)})
+        .then(this.setState({ requested: false }))
+        .catch(function (err) {
           //TODO #61: Centralize error output
           alert(err);
         });
@@ -45,21 +45,23 @@ class ClubGridItem extends Component {
     const isViewer = !isOwner && !isMember;
     const isRequester = this.props.club.requestIDs.includes(userID);
     return (
-      <Card className="col-3 group-container">
-        <Link id="group-link" to={`/clubpage/${this.props.club.id}`}>
-          {isOwner && <Card.Header className="header text-muted"> Owner </Card.Header>}
-          {isMember && <Card.Header className="header text-muted"> Member </Card.Header>}
-          {isViewer && <Card.Header className="header text-muted"> Viewer </Card.Header>}
-          <Card.Body>
-            <Card.Title id="group-name"> {this.props.club.name} </Card.Title>
-            <Card.Subtitle className="mb-2 text-muted" id="group-gbook"> Reading: {this.props.club.bookTitle} </Card.Subtitle>
-            <Card.Text id="group-description"> {this.props.club.description} </Card.Text>
-            {isViewer && !isRequester && <Button variant="primary" onClick={this.requestJoin}> Request to Join! </Button>}
-            {isRequester && <Button variant="danger" onClick={this.unRequestJoin}> Withdraw Request </Button>}
-          </Card.Body>
-          <Card.Footer className="footer text-muted"> {this.props.club.memberIDs.length} Members </Card.Footer>
-        </Link>
-      </Card>
+      <Col xs={12} sm={6} md={3}>
+        <Card className="col-3 group-container">
+          <Link id="group-link" to={`/clubpage/${this.props.club.id}`}>
+            {isOwner && <Card.Header className="header text-muted"> Owner </Card.Header>}
+            {isMember && <Card.Header className="header text-muted"> Member </Card.Header>}
+            {isViewer && <Card.Header className="header text-muted"> Viewer </Card.Header>}
+            <Card.Body>
+              <Card.Title id="group-name"> {this.props.club.name} </Card.Title>
+              <Card.Subtitle className="mb-2 text-muted" id="group-gbook"> Reading: {this.props.club.bookTitle} </Card.Subtitle>
+              <Card.Text id="group-description"> {this.props.club.description} </Card.Text>
+              {isViewer && !isRequester && <Button variant="primary" onClick={this.requestJoin}> Request to Join! </Button>}
+              {isRequester && <Button variant="danger" onClick={this.unRequestJoin}> Withdraw Request </Button>}
+            </Card.Body>
+            <Card.Footer className="footer text-muted"> {this.props.club.memberIDs.length} Members </Card.Footer>
+          </Link>
+        </Card>
+      </Col>
     );
   }
 
