344e4f2c2884cff9024abf4e1961136c7aed8887
Mauro Guerrero <mauroguerrero20@gmail.com>
2020-07-28 23:38:00 +0000

    Initial Commit and changes

--- capstone-frontend/src/components/Home.jsx
+++ capstone-frontend/src/components/Home.jsx
@@ -5,6 +5,8 @@ class Home extends Component {
   render() {
     return (
       <div>
+        <h1 className="text-center">Clubs</h1> 
+        <hr className="light-gray-border"/>
         <div> Your Books </div>
         <div> Your Lists </div>
         <div> Your Clubs </div>
