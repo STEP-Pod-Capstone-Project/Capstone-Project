61c1114398f84549e7c44ec90d792b85d7133f11
steven-solar <steven.j.solar@gmail.com>
2020-07-30 19:59:29 +0000

    links for prod

--- capstone-frontend/src/components/ClubPage.jsx
+++ capstone-frontend/src/components/ClubPage.jsx
@@ -44,7 +44,7 @@ class ClubPage extends Component {
       });
 
     if (this.state.club.gbookID.length > 0) {
-      await fetch(`https://8080-bfda3bef-a7ee-4ff4-91c6-c56fa0a00eba.ws-us02.gitpod.io/api/search?gbookId=${this.state.club.gbookID}`)
+      await fetch(`/api/search?gbookId=${this.state.club.gbookID}`)
         .then(response => response.json()).then(bookJson => this.setState({ book: bookJson[0] }))
         .catch(function (err) {
           //TODO #61: Centralize error output
@@ -52,14 +52,14 @@ class ClubPage extends Component {
         });
     }
 
-    await fetch(`https://8080-bfda3bef-a7ee-4ff4-91c6-c56fa0a00eba.ws-us02.gitpod.io/api/assignments?clubID=${this.state.club.id}`)
+    await fetch(`/api/assignments?clubID=${this.state.club.id}`)
       .then(response => response.json()).then(assignmentJson => this.setState({ assignments: assignmentJson }))
       .catch(function (err) {
         //TODO #61: Centralize error output
         alert(err);
       });
 
-    await fetch(`https://8080-bfda3bef-a7ee-4ff4-91c6-c56fa0a00eba.ws-us02.gitpod.io/api/user?id=${this.state.club.ownerID}`)
+    await fetch(`/api/user?id=${this.state.club.ownerID}`)
       .then(response => response.json()).then(ownerJson => this.setState({ owner: ownerJson }))
       .catch(function (err) {
         //TODO #61: Centralize error output
@@ -68,7 +68,7 @@ class ClubPage extends Component {
 
     this.setState({ members: [] });
     for (let i = 0; i < this.state.club.memberIDs.length; i++) {
-      await fetch(`https://8080-bfda3bef-a7ee-4ff4-91c6-c56fa0a00eba.ws-us02.gitpod.io/api/user?id=${this.state.club.memberIDs[i]}`)
+      await fetch(`/api/user?id=${this.state.club.memberIDs[i]}`)
         .then(response => response.json())
         .then(memberJson => memberJson && this.setState({ members: [...this.state.members, memberJson] }))
         .catch(function (err) {
@@ -91,7 +91,7 @@ class ClubPage extends Component {
       'whenCreated': (new Date()).toUTCString(), 
       'whenDue': dueDate
     };
-    fetch(`https://8080-bfda3bef-a7ee-4ff4-91c6-c56fa0a00eba.ws-us02.gitpod.io/api/assignments`, { method: 'post', body: JSON.stringify(data) })
+    fetch(`/api/assignments`, { method: 'post', body: JSON.stringify(data) })
       .then(response => response.json())
       .then(assignmentJson => {
         let assignments = this.state.assignments;
@@ -108,7 +108,7 @@ class ClubPage extends Component {
     let club = this.state.club;
     club.gbookID = gbookID;
     this.setState({ club });
-    await fetch(`https://8080-bfda3bef-a7ee-4ff4-91c6-c56fa0a00eba.ws-us02.gitpod.io/api/search?gbookId=${this.state.club.gbookID}`)
+    await fetch(`/api/search?gbookId=${this.state.club.gbookID}`)
       .then(response => response.json()).then(bookJson => this.setState({ book: bookJson[0] }))
       .catch(function (err) {
         //TODO #61: Centralize error output
@@ -147,7 +147,7 @@ class ClubPage extends Component {
             objectId={this.props.id}
             update={this.handleBookChange}
             text='Change the Club&quot;s Book'
-            putURL='https://8080-bfda3bef-a7ee-4ff4-91c6-c56fa0a00eba.ws-us02.gitpod.io/api/clubs'
+            putURL='/api/clubs'
             type='club'
             btnStyle='btn btn-primary mb-4 mt-4 mr-2' />
         }
